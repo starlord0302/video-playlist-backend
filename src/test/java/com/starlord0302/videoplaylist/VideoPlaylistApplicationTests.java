ce1242a9104b15fc662ef96d4952e0f591f30ce7
Károly Kovács <karoly0302@gmail.com>
2020-01-11 13:02:52 +0100

    feat(project): initialize project

---
+++ src/test/java/com/starlord0302/videoplaylist/VideoPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlord0302.videoplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoPlaylistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
